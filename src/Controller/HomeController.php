7a71ea64c43b56a5caa86b17907cf38b54aa8c67
Clement lemarie <big_m@live.fr>
2025-01-28 17:49:47 +0100

    twig and tet page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+// src/Controller/HomeController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends AbstractController
+{
+  #[Route('/')]
+  public function index(): Response
+  {
+    $number = random_int(0, 100);
+
+    return $this->render('pages/home.html.twig', [
+      'number' => $number,
+    ]);
+  }
+}
